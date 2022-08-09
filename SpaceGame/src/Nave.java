33a921c3c4ac964c3395bdb5a4678d9961b982a7
Johann Mayos Lopes da Silva <42842871+JohannMayos@users.noreply.github.com>
2022-08-09 18:59:39 -0400

    ship new attributes added

--- SpaceGame/src/Nave.java
+++ SpaceGame/src/Nave.java
@@ -3,20 +3,43 @@ public class Nave {
 	
 	private int x;
 	private int y;
+	private int lifes;
+	private int damage;
+
 	
 	public Nave() {
 		
 	}
 	
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	public int getLifes() {
+		return lifes;
+	}
+
+	public void setLifes(int lifes) {
+		this.lifes = lifes;
+	}
+
+	
 	public int getX() {
 		return x;
 	}
+	
 	public void setX(int x) {
 		this.x = x;
 	}
+	
 	public int getY() {
 		return y;
 	}
+	
 	public void setY(int y) {
 		this.y = y;
 	}
